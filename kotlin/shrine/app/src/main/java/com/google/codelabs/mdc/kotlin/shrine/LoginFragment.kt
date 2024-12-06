e031d00b1275e9ecb3d10eb0092abd42321781da
fdc.smpyn <40347173+dwardy0101@users.noreply.github.com>
2024-12-06 14:26:56 +0800

    mdc; commit 1

--- kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/LoginFragment.kt
+++ kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/LoginFragment.kt
@@ -1,25 +1,55 @@
 package com.google.codelabs.mdc.kotlin.shrine
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 /**
  * Fragment representing the login screen for Shrine.
  */
 class LoginFragment : Fragment() {
 
+    private var etPassword: TextInputEditText? = null
+    private var etPasswordTextInputLayout: TextInputLayout? = null
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-
-        // Snippet from "Navigate to the next Fragment" section goes here.
-
         val view = inflater.inflate(R.layout.shr_login_fragment, container, false)
+
+        etPassword = view.findViewById(R.id.password_edit_text)
+        etPasswordTextInputLayout = view.findViewById(R.id.password_text_input)
+
+        // Set an error if the password is less than 8 characters.
+        view.findViewById<View>(R.id.next_button).setOnClickListener {
+            if (!isPasswordValid(etPassword?.text)) {
+                etPasswordTextInputLayout?.error = getString(R.string.shr_error_password)
+            } else {
+                // Clear the error.
+                etPasswordTextInputLayout?.error = null
+                (activity as NavigationHost).navigateTo(ProductGridFragment(), false)
+            }
+        }
+
+        // Clear the error once more than 8 characters are typed.
+        etPassword?.setOnKeyListener { _, _, _ ->
+            if (isPasswordValid(etPassword?.text)) {
+                // Clear the error.
+                etPasswordTextInputLayout?.error = null
+            }
+            false
+        }
+
         return view
     }
 
     // "isPasswordValid" from "Navigate to the next Fragment" section method goes here
+    private fun isPasswordValid(text: Editable?): Boolean {
+        return text != null && text.length >= 8
+    }
 }
--- kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
+++ kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
@@ -2,15 +2,40 @@ package com.google.codelabs.mdc.kotlin.shrine
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import com.google.codelabs.mdc.kotlin.shrine.databinding.ShrProductGridFragmentBinding
 
 class ProductGridFragment : Fragment() {
+
+    private var _binding: ShrProductGridFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.shr_product_grid_fragment, container, false)
+        _binding = ShrProductGridFragmentBinding.inflate(inflater, container, false)
+
+        // Set up the toolbar.
+        if (activity is MainActivity) {
+            (activity as MainActivity).setSupportActionBar(binding.appBar)
+        }
+
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.shr_toolbar_menu, menu)
+        super.onCreateOptionsMenu(menu, menuInflater)
     }
 }
