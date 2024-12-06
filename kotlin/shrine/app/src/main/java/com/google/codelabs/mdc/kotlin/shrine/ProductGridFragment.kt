3168dc7d331a79b66bededb1c0c34bc239fc8ed3
fdc.smpyn <40347173+dwardy0101@users.noreply.github.com>
2024-12-06 16:18:14 +0800

    mdc; commit 2
    code lab complete

--- kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
+++ kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
@@ -1,41 +1,85 @@
 package com.google.codelabs.mdc.kotlin.shrine
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.codelabs.mdc.kotlin.shrine.databinding.ShrProductGridFragmentBinding
+import com.google.codelabs.mdc.kotlin.shrine.network.ProductEntry
+import com.google.codelabs.mdc.kotlin.shrine.staggeredgridlayout.StaggeredProductCardRecyclerViewAdapter
 
 class ProductGridFragment : Fragment() {
 
     private var _binding: ShrProductGridFragmentBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setHasOptionsMenu(true)
-    }
-
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         _binding = ShrProductGridFragmentBinding.inflate(inflater, container, false)
 
-        // Set up the toolbar.
-        if (activity is MainActivity) {
-            (activity as MainActivity).setSupportActionBar(binding.appBar)
+        (activity as MainActivity).setSupportActionBar(binding.appBar)
+
+        // Set up the RecyclerView
+        binding.recyclerView.setHasFixedSize(true)
+        binding.recyclerView.layoutManager = GridLayoutManager(context, 2, RecyclerView.VERTICAL, false)
+        val adapter = StaggeredProductCardRecyclerViewAdapter(
+            ProductEntry.initProductEntryList(resources))
+        binding.recyclerView.adapter = adapter
+        val largePadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing)
+        val smallPadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing_small)
+        binding.recyclerView.addItemDecoration(ProductGridItemDecoration(largePadding, smallPadding))
+
+        // Set cut corner background for API 23+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            binding.productGrid.background = AppCompatResources.getDrawable(requireContext(), R.drawable.shr_product_grid_background_shape)
         }
 
-        return view
+        binding.appBar.setNavigationOnClickListener(NavigationIconClickListener(
+            requireActivity(),
+            binding.productGrid,
+            AccelerateDecelerateInterpolator(),
+            ContextCompat.getDrawable(requireContext(), R.drawable.shr_branded_menu),
+            ContextCompat.getDrawable(requireContext(), R.drawable.shr_close_menu)))
+
+        return binding.root
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, menuInflater: MenuInflater) {
-        menuInflater.inflate(R.menu.shr_toolbar_menu, menu)
-        super.onCreateOptionsMenu(menu, menuInflater)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Set up the toolbar.
+        val menuHost = requireActivity()
+
+        menuHost.addMenuProvider(object : MenuProvider {
+
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.shr_toolbar_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.search -> {
+                        // Handle action
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 }
